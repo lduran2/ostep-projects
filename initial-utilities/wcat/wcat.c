57efdcebe77c7a8bb3c318f2be5a11acfd2b495b
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2020-01-23 01:09:00 -0500

    wget version 2.0

--- initial-utilities/wcat/wcat.c
+++ initial-utilities/wcat/wcat.c
@@ -1,35 +1,63 @@
+/**
+ * ostep-projects/initial-utilities/wcat/wcat.c
+ * Concatenates the contents of file specified by the command-line
+ * arguments to standard output. 
+ * by: Leomar Duran <https://github.com/lduran2>
+ * for: CIS 3207 Assignment 0
+ * date: 2020-01-23
+ * due: 2020-01-30
+ */
 #include <stdlib.h> /* malloc */
-#include <stdio.h> /* FILE, fopen, fgets, fclose */
+#include <stdio.h> /* FILE, fopen, fgets, fprintf, fclose */
 
 int
 main(int argc, char** argv)
 {
 	/* prototypes */
-	int wcatf(FILE*); /* single file concatenate */
+	void wcpf(FILE*, FILE*); /* file-to-file copy */
 
 	size_t k; /* file counter */
 	FILE* file; /* the file to read */
 
+	//FILE* outstream;
+	//outstream = fopen("outstream", "a");
+
 	/* for each file */
 	/* note that argv[0] is the command call of this program */
-	for (k = 1, ++argv; (k < argc); ++k, ++argv)
+	for (k = 1; (k != argc); ++k)
 	{
-		/* concatenate the file */
-		printf("reading %s . . .", *argv);
-		file = fopen(*argv, "r");
-		wcatf(file);
+		/* open the current file */
+		file = fopen(argv[k], "r");
+		/* concatenate the current file */
+		wcpf(file, stdout);
+		//wcpf(file, outstream);
+		/* close the current file */
+		fclose(file);
 	} /* end for (; k < argc; ); */
+
+	//fclose(outstream);
+
+	/* no errors */
 	return 0;
 } /* end main(int, char**); */
 
-int wcatf(FILE* file)
+/**
+ * Copies the contents of infile to outfile.
+ * params:
+ *   infile  : FILE* = wherefrom to read
+ *   outfile : FILE* = whereto to write
+ */
+void wcpf(FILE* infile, FILE* outfile)
 {
-	const size_t LEN = 255; /* number of chracters to read at a time */
-	char* line = ((char*) malloc(LEN * sizeof(char))); /* current line */
-	while (NULL != fgets(line, 255, file))
+	const size_t LEN = 255; /* number of characters to read at a time */
+	char* line = ((char*) malloc(LEN * sizeof(char))); /* current line buffer */
+	/* read to line buffer while not EOF */
+	while (NULL != fgets(line, 255, infile))
 	{
-		printf("%s", line);
-	}
-	return 0;
-}
+		/* print the line to the outfile */
+		fprintf(outfile, "%s", line);
+	} /* while (NULL != fgets(, , infile); */
+	/* deallocate the line buffer */
+	free(line);
+} /* end wcpf(FILE*, FILE*); */
 
