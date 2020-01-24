ed10c8a96a1209b92855f5c8384eae9822d1b9ba
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2020-01-23 22:18:07 -0500

    wcat v2.1 polished

--- initial-utilities/wcat/wcat.c
+++ initial-utilities/wcat/wcat.c
@@ -9,6 +9,7 @@
  */
 #include <stdlib.h> /* malloc */
 #include <stdio.h> /* FILE, fopen, fgets, fprintf, fclose */
+#include <stdbool.h> /* bool */
 
 int
 main(int argc, char** argv)
@@ -18,27 +19,34 @@ main(int argc, char** argv)
 
 	size_t k; /* file counter */
 	FILE* file; /* the file to read */
-
-	//FILE* outstream;
-	//outstream = fopen("outstream", "a");
+	bool is_open = true; /* whether the file was opened, initially true */
 
 	/* for each file */
 	/* note that argv[0] is the command call of this program */
-	for (k = 1; (k != argc); ++k)
+	/* thus ignored */
+	for (k = 1; (is_open && (k != argc)); ++k)
 	{
 		/* open the current file */
 		file = fopen(argv[k], "r");
-		/* concatenate the current file */
-		wcpf(file, stdout);
-		//wcpf(file, outstream);
-		/* close the current file */
-		fclose(file);
-	} /* end for (; k < argc; ); */
+		/* check if able to open file */
+		is_open = (NULL != file);
 
-	//fclose(outstream);
+		/* if able to open */
+		if (is_open) {
+			/* concatenate the current file */
+			wcpf(file, stdout);
+			/* close the current file */
+			fclose(file);
+		} /* end if (is_open) */
 
-	/* no errors */
-	return 0;
+		else {
+			/* print an error message */
+			fprintf(stderr, "wcat: cannot open file\n");
+		} /* end (is_open) else */
+	} /* end for (; (is_open && (k < argc)); ); */
+
+	/* no errors if last file opened, error code 1 otherwise */
+	return ((is_open) ? 0 : 1);
 } /* end main(int, char**); */
 
 /**
@@ -50,13 +58,15 @@ main(int argc, char** argv)
 void wcpf(FILE* infile, FILE* outfile)
 {
 	const size_t LEN = 255; /* number of characters to read at a time */
-	char* line = ((char*) malloc(LEN * sizeof(char))); /* current line buffer */
+	/* current line buffer */
+	char* line = ((char*) malloc(LEN * sizeof(char)));
+
 	/* read to line buffer while not EOF */
 	while (NULL != fgets(line, 255, infile))
 	{
 		/* print the line to the outfile */
 		fprintf(outfile, "%s", line);
-	} /* while (NULL != fgets(, , infile); */
+	} /* end while (NULL != fgets(, , infile); */
 	/* deallocate the line buffer */
 	free(line);
 } /* end wcpf(FILE*, FILE*); */
