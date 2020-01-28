84fc7a916b9355eaba3c182663492749967dd744
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2020-01-28 10:50:10 -0500

    wgrep v2.0

--- initial-utilities/wcat/wcat.c
+++ initial-utilities/wcat/wcat.c
@@ -15,11 +15,15 @@ int
 main(int argc, char** argv)
 {
 	/* prototypes */
-	void wcpf(FILE*, FILE*); /* file-to-file copy */
+	void wcpf(FILE*, FILE*, const size_t, char*); /* file-to-file copy */
 
 	size_t k; /* file counter */
 	FILE* file; /* the file to read */
 	bool is_open = true; /* whether the file was opened, initially true */
+
+	const size_t CBUFF_LEN = 4096; /* number of characters to read at a time */
+	/* character buffer */
+	char* cbuff = ((char*) malloc(CBUFF_LEN * sizeof(char)));
 
 	/* for each file */
 	/* note that argv[0] is the command call of this program */
@@ -34,7 +38,7 @@ main(int argc, char** argv)
 		/* if able to open */
 		if (is_open) {
 			/* concatenate the current file */
-			wcpf(file, stdout);
+			wcpf(file, stdout, CBUFF_LEN, cbuff);
 			/* close the current file */
 			fclose(file);
 		} /* end if (is_open) */
@@ -45,6 +49,9 @@ main(int argc, char** argv)
 		} /* end (is_open) else */
 	} /* end for (; (is_open && (k < argc)); ); */
 
+	/* deallocate the character buffer */
+	free(cbuff);
+
 	/* no errors if last file opened, error code 1 otherwise */
 	return ((is_open) ? 0 : 1);
 } /* end main(int, char**); */
@@ -52,22 +59,18 @@ main(int argc, char** argv)
 /**
  * Copies the contents of infile to outfile.
  * params:
- *   infile  : FILE* = wherefrom to read
- *   outfile : FILE* = whereto to write
+ *   infile    : FILE*        = wherefrom to read
+ *   outfile   : FILE*        = whereto to write
+ *   CBUFF_LEN : const size_t = size of the character buffer
+ *   cbuff     : char*        = the character buffer
  */
-void wcpf(FILE* infile, FILE* outfile)
+void wcpf(FILE* infile, FILE* outfile, const size_t CBUFF_LEN, char* cbuff)
 {
-	const size_t LEN = 255; /* number of characters to read at a time */
-	/* current line buffer */
-	char* line = ((char*) malloc(LEN * sizeof(char)));
-
 	/* read to line buffer while not EOF */
-	while (NULL != fgets(line, 255, infile))
+	while (NULL != fgets(cbuff, CBUFF_LEN, infile))
 	{
 		/* print the line to the outfile */
-		fprintf(outfile, "%s", line);
+		fprintf(outfile, "%s", cbuff);
 	} /* end while (NULL != fgets(, , infile); */
-	/* deallocate the line buffer */
-	free(line);
 } /* end wcpf(FILE*, FILE*); */
 
